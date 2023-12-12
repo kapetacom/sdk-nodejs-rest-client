029f573a7e8d76350987deb77468c711d50ef744
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-12 18:44:54 +0100

    feat: Refactored to seperate request from client (#8)
    BREAKING CHANGE: Refactoring to make it possible to interact with request

--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ import Request, { Response } from 'request';
 
 const SERVICE_TYPE = 'rest';
 
-type RequestArgumentTransport = 'path' | 'header' | 'body' | 'query' | 'PATH' | 'HEADER' | 'BODY' | 'QUERY';
-type RequestMethod =
+export type RequestArgumentTransport = 'path' | 'header' | 'body' | 'query' | 'PATH' | 'HEADER' | 'BODY' | 'QUERY';
+export type RequestMethod =
     | 'GET'
     | 'POST'
     | 'DELETE'
@@ -46,7 +46,7 @@ export interface Result {
     body: any;
 }
 
-export class RestError extends Error {
+export class RestClientError extends Error {
     public readonly response: Response;
     public readonly statusCode: number;
 
@@ -64,69 +64,112 @@ const JSONStringifyReplacer = function(this:any, key:string, value:any) {
     return value;
 }
 
-export class RestClient {
-    private readonly _resourceName: string;
-    private _ready: boolean = false;
-    private _baseUrl: string;
+export class RestClientRequest<ReturnType = any> {
+    private readonly _baseUrl: string;
+    private readonly _path: string;
+    private readonly _method: RequestMethod;
+    private readonly _requestArguments: RequestArgument[];
+    private readonly _headers: { [key: string]: string } = {};
 
-    /**
-     * Initialise rest client for service.
-     *
-     * @param {string} resourceName
-     */
-    constructor(resourceName: string) {
-        this._resourceName = resourceName;
-        this._baseUrl = `http://${resourceName.toLowerCase()}`;
-
-        Config.onReady(async (provider) => {
-            await this.init(provider);
-        });
-    }
-
-    /**
-     * Called automatically during startup sequence.
-     *
-     * @param {ConfigProvider} provider
-     * @return {Promise<void>}
-     */
-    async init(provider: ConfigProvider) {
-        this._baseUrl = await provider.getServiceAddress(this._resourceName, SERVICE_TYPE);
-        this._ready = true;
-
-        if (!this._baseUrl.endsWith('/')) {
-            this._baseUrl += '/';
-        }
-
-        console.log('REST client ready for %s --> %s', this._resourceName, this._baseUrl);
-    }
-
-    /**
-     * Executes a request to the specified path using the specified method.
-     *
-     * @param {RequestMethod} method - The HTTP request method to use for the request.
-     * @param {string} path - The path of the resource to request.
-     * @param {RequestArgument[]} requestArguments - The array of request arguments.
-     * @returns {Promise<ReturnType | null>} The result of the request, or null if the response status is 404.
-     */
-    execute<ReturnType = any>(method: RequestMethod, path: string, requestArguments: RequestArgument[]) {
-        if (!this._ready) {
-            throw new Error('Client not ready yet');
-        }
-
+    constructor(baseUrl: string, method: RequestMethod, path: string, requestArguments: RequestArgument[]) {
         while (path.startsWith('/')) {
             path = path.substring(1);
         }
 
-        const url = this._baseUrl + path;
+        this._baseUrl = baseUrl;
+        this._path = path;
+        this._method = method;
+        this._requestArguments = requestArguments;
+    }
 
+    public get url() {
+        return this._baseUrl + this._path;
+    }
+
+    public get method() {
+        return this._method;
+    }
+
+    public get arguments() {
+        return [...this._requestArguments];
+    }
+
+    public get headers() {
+        return {
+            ...this._headers,
+        };
+    }
+
+    public hasHeader(name: string) {
+        return this._headers[name] !== undefined;
+    }
+
+    public withHeader(name: string, value: string|undefined) {
+        if (!value) {
+            delete this._headers[name];
+            return this;
+        }
+        this._headers[name] = value;
+        return this;
+    }
+
+
+    public withAuthorization(auth: string|undefined) {
+        return this.withHeader('Authorization', auth);
+    }
+    public withBearerToken(token: string|undefined) {
+        return this.withAuthorization(`Bearer ${token}`);
+    }
+
+    public withContentType(contentType: string|undefined) {
+        return this.withHeader('Content-Type', contentType);
+    }
+
+    public call():Promise<ReturnType|null> {
+        const opts = this.createOptions();
+        return new Promise<ReturnType | null>((resolve, reject) => {
+            Request(opts, function (err: Error, response: Response, body: any) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                if (
+                    typeof body === 'string' &&
+                    response.headers['content-type'] &&
+                    response.headers['content-type']?.startsWith('application/json')
+                ) {
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+                        // Ignore
+                    }
+                }
+
+                if (response.statusCode > 399 && response.statusCode !== 404) {
+                    reject(new RestClientError(body.error || 'Unknown error', response));
+                    return;
+                }
+
+                if (response.statusCode === 404) {
+                    resolve(null);
+                    return;
+                }
+
+                resolve(body);
+            });
+        });
+    }
+
+    protected createOptions() {
         const query: string[] = [];
         const opts: RequestOptions = {
-            method,
-            url,
-            headers: {},
+            method: this.method,
+            url: this.url,
+            headers: {...this._headers},
         };
 
-        requestArguments.forEach((requestArgument) => {
+        this._requestArguments.forEach((requestArgument) => {
             const transport = requestArgument.transport?.toLowerCase() as Lowercase<RequestArgumentTransport>;
             switch (transport) {
                 case 'path':
@@ -155,38 +198,94 @@ export class RestClient {
         if (query.length > 0) {
             opts.url += '?' + query.join('&');
         }
+        return opts;
+    }
 
-        return new Promise<ReturnType | null>((resolve, reject) => {
-            Request(opts, function (err: Error, response: Response, body: any) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
+}
 
-                if (
-                    typeof body === 'string' &&
-                    response.headers['content-type'] &&
-                    response.headers['content-type']?.startsWith('application/json')
-                ) {
-                    try {
-                        body = JSON.parse(body);
-                    } catch (e) {
-                        // Ignore
-                    }
-                }
+export class RestClient {
+    private readonly _resourceName: string;
+    private _ready: boolean = false;
+    private _baseUrl: string;
+    private _fixedHeaders: { [key: string]: string } = {};
 
-                if (response.statusCode > 399 && response.statusCode !== 404) {
-                    reject(new RestError(body.error || 'Unknown error', response));
-                    return;
-                }
+    /**
+     * Initialise rest client for service.
+     */
+    public constructor(resourceName: string) {
+        this._resourceName = resourceName;
+        this._baseUrl = `http://${resourceName.toLowerCase()}`;
 
-                if (response.statusCode === 404) {
-                    resolve(null);
-                    return;
-                }
-
-                resolve(body);
-            });
+        Config.onReady(async (provider) => {
+            await this.init(provider);
         });
+    }
+
+
+    /**
+     * Called automatically during startup sequence.
+     */
+    private async init(provider: ConfigProvider) {
+        this._baseUrl = await provider.getServiceAddress(this._resourceName, SERVICE_TYPE);
+        this._ready = true;
+
+        if (!this._baseUrl.endsWith('/')) {
+            this._baseUrl += '/';
+        }
+
+        console.log('REST client ready for %s --> %s', this._resourceName, this._baseUrl);
+    }
+
+    public get baseUrl() {
+        return this._baseUrl;
+    }
+
+    public withHeader(name: string, value: string|undefined) {
+        if (!value) {
+            delete this._fixedHeaders[name];
+            return this;
+        }
+        this._fixedHeaders[name] = value;
+        return this;
+    }
+
+    public withContentType(contentType: string|undefined) {
+        return this.withHeader('Content-Type', contentType);
+    }
+
+    public withAuthorization(auth: string|undefined) {
+        return this.withHeader('Authorization', auth);
+    }
+    public withBearerToken(token: string|undefined) {
+        return this.withAuthorization(`Bearer ${token}`);
+    }
+
+    public create<ReturnType = any>(method: RequestMethod, path: string, requestArguments: RequestArgument[]):RestClientRequest<ReturnType> {
+        if (!this._ready) {
+            throw new Error('Client not ready yet');
+        }
+
+        const request = new RestClientRequest<ReturnType>(this._baseUrl, method, path, requestArguments);
+
+        Object.keys(this._fixedHeaders).forEach((key) => {
+            request.withHeader(key, this._fixedHeaders[key]);
+        });
+
+        this.afterCreate(request);
+
+        return request;
+    }
+
+    protected afterCreate(request: RestClientRequest):void {
+        // Override this method to add additional headers or similar to all requests
+    }
+
+    /**
+     * Executes a request to the specified path using the specified method.
+     */
+    public execute<ReturnType = any>(method: RequestMethod, path: string, requestArguments: RequestArgument[]) {
+        const request = this.create<ReturnType>(method, path, requestArguments);
+
+        return request.call()
     }
 }
